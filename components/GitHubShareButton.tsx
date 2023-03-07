33ffa4115cc745e63ca21481c0eb04e020b5d19e
0x1984-0x1976 <109565492+0x1984@users.noreply.github.com>
2023-03-07 20:22:40 +0800

    Update GitHubShareButton.tsx
    add twitter link

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,11 +5,11 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://twitter.com/AllRecode'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
-      aria-label='View source on GitHub'
+      aria-label='Twitter Account of Allrecode'
     >
       <svg
         width='80'
